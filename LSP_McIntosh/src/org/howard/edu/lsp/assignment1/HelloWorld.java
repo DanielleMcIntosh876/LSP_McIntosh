7b98701ba8915b36b6f5ebbe5b2e1ae346597f98
Danielle McIntosh <danielle.mcintosh@bison.howard.edu>
2022-09-01 22:30:21 -0400

    This is my commit for assignment #1.

---
+++ LSP_McIntosh/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -0,0 +1,17 @@
+package org.howard.edu.lsp.assignment1;
+
+/**
+ * Danielle McIntosh
+ * 
+ * @author danielle
+ *
+ */
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Large Scale Programming!!");
+
+	}
+
+}
